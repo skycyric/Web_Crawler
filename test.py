36a89923c0b338212cc1862bb0a643e2f87f7974
skycyric <skycyric@gmail.com>
2022-01-25 16:03:52 +0800

    commit

--- test.py
+++
@@ -1,8 +0,0 @@
-import imp
-import requests
-from bs4 import BeautifulSoup
-
-playlist = []
-url = input("Enter the Youtube Playlist URL : ")  # Takes the Playlist Link
-data = requests.get(url)
-soup = bs4.BeautifulSoup(data.text, 'html.parser')
